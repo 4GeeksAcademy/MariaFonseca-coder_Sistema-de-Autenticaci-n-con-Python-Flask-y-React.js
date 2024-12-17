db23110505a0102d5b2e0446b2e2f1a74c0c82f9
María José Fonseca Castro <mfonsecac052@gmail.com>
2024-12-17 03:29:45 +0000

    tabla User lista

--- src/api/models.py
+++ src/api/models.py
@@ -4,8 +4,11 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(20), unique=False, nullable=False)
+    apellidos = db.Column(db.String(20), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.String(120), unique=False, nullable=False)
+    telefono = db.Column(db.String(20), unique=True, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
@@ -14,6 +17,8 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "nombre": self.nombre,
+            "apellidos": self.apellidos,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "telefono": self.telefono,
         }
