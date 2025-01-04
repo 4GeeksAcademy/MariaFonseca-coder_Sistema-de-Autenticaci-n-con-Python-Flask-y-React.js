0c5c0608ff8761a5b917cf941f057f37a61e1edd
María José Fonseca Castro <mfonsecac052@gmail.com>
2025-01-04 23:41:11 +0000

    login frontend preparado

--- src/front/js/pages/login.jsx
+++ src/front/js/pages/login.jsx
@@ -5,23 +5,43 @@ import { Context } from "../store/appContext";
 
 export const Login = () => {
     const { store, actions } = useContext(Context);
+    const [data, setData] = useState({
+        "email": "",
+        "password": ""
+    })
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setData(prevData => ({
+            ...prevData, [name]: value
+        }))
+    }
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+        actions.login(data.email, data.password)
+    }
 
     return (
         <div className="container">
-            <form>
+            <form onSubmit={handleLogin}>
+
                 <div className="row mb-3">
                     <label for="inputEmail3" className="col-sm-2 col-form-label">Email</label>
                     <div className="col-sm-10">
-                        <input type="email" className="form-control" id="inputEmail3" />
+                        <input type="email" className="form-control" id="inputEmail3" name="email" value={data.email} onChange={handleChange} />
                     </div>
                 </div>
+
                 <div className="row mb-3">
                     <label for="inputPassword3" className="col-sm-2 col-form-label">Password</label>
                     <div className="col-sm-10">
-                        <input type="password" className="form-control" id="inputPassword3" />
+                        <input type="password" className="form-control" id="inputPassword3" name="password" value={data.password} onChange={handleChange} />
                     </div>
                 </div>
-                <button type="submit" className="btn btn-primary">Sign in</button>
+
+                <button type="submit" className="btn btn-primary">Login</button>
+
             </form>
         </div>
     );
