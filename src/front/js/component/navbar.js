79050dabd8f4529617eb3db81b972551c8833e07
María José Fonseca Castro <mfonsecac052@gmail.com>
2025-01-09 19:10:00 +0000

    se creó archivo de vista a signup

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -8,7 +8,11 @@ export const Navbar = () => {
 				<Link to="/">
 					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
 				</Link>
-				<div className="ml-auto">
+
+				<div className="d-flex justify-content-end align-items-center">
+					<Link to="/signup" className="me-2">
+						<button className="btn btn-primary">Sign Up</button>
+					</Link>
 					<Link to="/login">
 						<button className="btn btn-primary">Login</button>
 					</Link>
---
+++ src/front/js/pages/signup.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+
+import { Context } from "../store/appContext";
+
+export const SignUp = () => {
+    const { store, actions } = useContext(Context);
+    const [data, setData] = useState({
+        "email": "",
+        "password": ""
+    })
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setData(prevData => ({
+            ...prevData, [name]: value
+        }))
+    }
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+        actions.login(data.email, data.password)
+    }
+
+    return (
+        <div className="container">
+            <form onSubmit={handleLogin}>
+
+                <div className="row mb-3">
+                    <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email</label>
+                    <div className="col-sm-10">
+                        <input type="email" className="form-control" id="inputEmail3" name="email" value={data.email} onChange={handleChange} />
+                    </div>
+                </div>
+
+                <div className="row mb-3">
+                    <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Password</label>
+                    <div className="col-sm-10">
+                        <input type="password" className="form-control" id="inputPassword3" name="password" value={data.password} onChange={handleChange} />
+                    </div>
+                </div>
+
+                <button type="submit" className="btn btn-primary">Login</button>
+
+            </form>
+        </div>
+    );
+};
